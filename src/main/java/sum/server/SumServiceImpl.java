f452e06bea308234bc69fbce845a0b29e4924a36
Takuya Shimada <gct3860@gmail.com>
2022-11-21 13:54:17 +0900

    [Exercise] Sum API (#1)
    * add sum.proto
    
    * add SumServer
    
    * add SumServiceImpl
    
    * add SumClient
    
    * add newline ending

---
+++ src/main/java/sum/server/SumServiceImpl.java
@@ -0,0 +1,14 @@
+package sum.server;
+
+import com.proto.sum.SumRequest;
+import com.proto.sum.SumResponse;
+import com.proto.sum.SumServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class SumServiceImpl extends SumServiceGrpc.SumServiceImplBase {
+    @Override
+    public void sum(SumRequest request, StreamObserver<SumResponse> responseObserver) {
+        responseObserver.onNext(SumResponse.newBuilder().setResult(request.getFirst() + request.getSecond()).build());
+        responseObserver.onCompleted();
+    }
+}
