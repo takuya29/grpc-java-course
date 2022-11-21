21f45ce53c7b8343216533a0097639f527dfbde7
Takuya Shimada <gct3860@gmail.com>
2022-11-21 11:11:33 +0900

    add greet endpoint

--- src/main/java/greeting/client/GreetingClient.java
+++ src/main/java/greeting/client/GreetingClient.java
@@ -1,17 +1,40 @@
 package greeting.client;
 
+import com.proto.greeting.GreetingRequest;
+import com.proto.greeting.GreetingResponse;
+import com.proto.greeting.GreetingServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
 public class GreetingClient {
 
+    private static void doGreet(ManagedChannel channel) {
+        System.out.println("enter doGreet");
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+        GreetingResponse response = stub.greet(GreetingRequest.newBuilder().setFirstName("Takuya").build());
+
+        System.out.println("Greeting: " + response.getResult());
+    }
+
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Need one argument to work");
+            return;
+        }
+
         ManagedChannel channel = ManagedChannelBuilder
                 .forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
 
         // do something
+        switch (args[0]) {
+            case "greet":
+                doGreet(channel);
+                break;
+            default:
+                System.out.println("Keyword invalid: " + args[0]);
+        }
 
         System.out.println("Shutting down");
         channel.shutdown();
--- src/main/java/greeting/server/GreetingServer.java
+++ src/main/java/greeting/server/GreetingServer.java
@@ -10,7 +10,10 @@ public class GreetingServer {
     public static void main(String[] args) throws IOException, InterruptedException {
         int port = 50051;
 
-        Server server = ServerBuilder.forPort(port).build();
+        Server server = ServerBuilder
+                .forPort(port)
+                .addService(new GreetingServerImpl())
+                .build();
 
         server.start();
         System.out.println("Sever started");
